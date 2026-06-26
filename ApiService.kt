abba5755f0279f44bbf94b53fea1cdb206ae8459
Bb <newtest2354@gmail.com>
2026-06-26 22:30:21 +0330

    Delete ApiService.kt

--- ApiService.kt
+++
@@ -1,9 +0,0 @@
-package com.arista.client.network
-
-import retrofit2.http.GET
-import retrofit2.http.Url
-
-interface ApiService {
-    @GET
-    suspend fun getConfigs(@Url url: String): String
-}
